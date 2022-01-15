eed12a31cde646008c6e4b5a955daacafa05d09f
Frank Tang <roadtang@gmail.com>
2022-01-15 23:13:54 +0800

    pyxel -  readme

---
+++ pip/pyxel/readme001.py
@@ -0,0 +1,18 @@
+import pyxel
+
+pyxel.init(160, 120)
+
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+def draw():
+    pyxel.rect(10, 10, 20, 20, 11)
+
+
+pyxel.run(update, draw)
+
+
+
+
---
+++ pip/pyxel/readme003.py
@@ -0,0 +1,12 @@
+
+
+#show function displays the screen and waits until the Esc key is pressed.
+
+
+import pyxel
+pyxel.init(120, 120)
+pyxel.cls(1)
+pyxel.circb(60, 60, 40, 7)
+pyxel.show()
+
+
