e2807247bfe6ebbd0e784616534b6ed92731adf0
Frank Tang <roadtang@gmail.com>
2022-06-19 16:11:24 +0800

    pip - unitest

---
+++ pip/unittest/test_unittest.py
@@ -0,0 +1,34 @@
+
+
+import unittest
+from six import print_
+
+class IntegerArithmeticTestCase(unittest.TestCase):
+    def testAdd(self):
+        self.assertEqual((1+2), 3)
+        self.assertEqual(0+1, 1)
+
+    def testMultiply(self):
+        self.assertEqual((0 * 10), 0)
+        self.assertEqual((5 * 8), 40)
+
+
+class Case1(unittest.TestCase):
+    
+    def setUp(self):
+        print_('setUp')
+
+    def tearDown(self):
+        print_('tearDown')
+
+
+    def test1(self):
+        print_("test1")
+
+    def test2(self):
+        print_("test2")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
