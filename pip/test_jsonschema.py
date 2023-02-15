799ba3355c33005f5b27e2f914b33dbbb926f46a
Frank Tang <roadtang@gmail.com>
2023-02-15 09:43:19 +0800

    pip - jsonschema

---
+++ pip/test_jsonschema.py
@@ -0,0 +1,17 @@
+
+
+from jsonschema import validate
+
+schema = {
+    "type": "object",
+    "properties": {
+        "price" : { "type": "number"},
+        "name": {"type": "string"},
+    },
+}
+
+validate(instance={"name": "Eggs", "price" : 34.99}, schema=schema)
+
+validate(instance={"name": "Eggs", "price": "Invalid"}, schema=schema)
+
+
